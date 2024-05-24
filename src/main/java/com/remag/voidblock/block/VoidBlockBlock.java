2c6f7e59d0b4cde1c366db85a84bdc57ab49edea
RemagOfficial <lloydfb456@gmail.com>
2024-05-24 17:06:57 +0100

    made void block actually solid

--- src/main/java/com/remag/voidblock/block/VoidBlockBlock.java
+++ src/main/java/com/remag/voidblock/block/VoidBlockBlock.java
@@ -1,16 +1,12 @@
 package com.remag.voidblock.block;
 
-import net.minecraft.client.model.EntityModel;
-import net.minecraft.client.renderer.entity.layers.RenderLayer;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 
 public class VoidBlockBlock extends Block {
 
     public VoidBlockBlock() {
-        super(BlockBehaviour.Properties.copy(Blocks.END_PORTAL)
+        super(BlockBehaviour.Properties.copy(Blocks.AMETHYST_BLOCK)
                 .strength(3f, 10f)
                 .sound(SoundType.AMETHYST));
     }
